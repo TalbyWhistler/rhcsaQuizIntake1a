0973fbd839b840ae701dfcc116f97dc944204ef9
TalbyWhistler <000748591@mohawkcollege.ca>
2026-07-07 12:42:04 -0400

    fixed small issue with null values

--- php/rhIntake_operations.php
+++ php/rhIntake_operations.php
@@ -45,8 +45,8 @@ function checkIfLoggedIn()
     $cookieUsername=$_COOKIE["linuxLab"]??'';
     $jsonValues=json_decode($cookieValues,true);
   //  return json_encode($jsonValues);
-    $loggedInUser=$jsonValues["username"];
-    $loggedInToken=$jsonValues["token"];
+    $loggedInUser=$jsonValues["username"]??'';
+    $loggedInToken=$jsonValues["token"]??'';
   //  return json_encode($jsonValues["username"]);
    // $cookieUsername=$_COOKIE["linuxLab"];
    // $cookieToken=$_COOKIE["linuxLab"]["token"]??'';
