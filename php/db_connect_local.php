bf695520922791c7f7479c18e32b41b9757823ec
TalbyWhistler <000748591@mohawkcollege.ca>
2026-07-11 20:39:11 -0400

    up to date jul 11

---
+++ php/db_connect_local.php
@@ -0,0 +1,31 @@
+<?php 
+//$servername="localhost";
+//$username = "sa000748591";
+//$password = "Sa_19780531";
+//$dbName = "sa000748591";
+
+$servername="localhost";
+$servername="%waters";
+$username = "webUser1";
+$password = "watersWeb";
+$dbName = "redhat0";
+
+
+$conn = new mysqli($servername,$username,$password,$dbName);
+$returnValue=false;
+if ($conn->connect_error)
+    {
+       // echo "</br>connect false";
+       $returnValue=false;
+    }
+    else 
+        {
+           // echo "</br>connect true";
+           $returnValue=true;
+        }
+
+return $returnValue;
+
+?>
+
+?>
